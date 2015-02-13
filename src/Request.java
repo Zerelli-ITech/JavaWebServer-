05e1b92c6bfe85be22e8eeef5be873a2933bcef9
zerelli <sezerelli@pc12809-bes.dynamic.besancon.parkeon.com>
2015-02-13 17:40:56 +0100

    resquest: add constructor

--- src/Request.java
+++ src/Request.java
@@ -1,4 +1,16 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 
 public class Request {
+	List<String> headers = new ArrayList<String>();
+	
+	public Request()throws IOException {
+		// TODO Auto-generated constructor stub
+	}
+	
 
 }
